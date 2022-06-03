43fc0d3b51cf65c94600a5efeb686c679c3c562e
Tiago <ztiagok@gmail.com>
2022-06-03 17:43:37 -0500

    c5 - Requisito 4 iniciado

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,13 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = (id) => {
+  const itemUrl = `https://api.mercadolibre.com/items/${id}`;
+
+  return fetch(itemUrl)
+  .then((response) => response.json())
+  .then((data) => data)
+  .catch((error) => error);
 };
+
+console.log(fetchItem('MLB1615760527'));
 
 if (typeof module !== 'undefined') {
   module.exports = {
